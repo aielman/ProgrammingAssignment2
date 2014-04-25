4668832b303b321e1e8d130f55cf53d50423a14a
aielman <vinod.estel@gmail.com>
2014-04-26 03:10:47 +0530

    Cachematrix.R stub file is updated with the functions makeCachematrix and cacheSolve. The script is fully functional for all non-singular matrices. The scripts accepts a non-singular matrix and returns the inverse of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix accepts a matrix and returns a list of 4 items. It adds caching
+## features to the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(z) inverse <<- z
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+
+## This function returns the inverse of a non singular matrix. If the inverse is
+## computed in a previous iteration, the function retrieves the value from cache, else
+## the inverse is computed, stored in the matrix and returns the inverse to the calling
+## environment
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        #print("Received the result from Cache")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
 }
